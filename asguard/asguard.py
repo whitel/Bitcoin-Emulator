02dc95d26ea0961032c197672983cb9ff73e1a2f
whitel <heriskent@gmail.com>
2022-11-16 22:30:16 +0800

    rename malas to asguard

--- asguard/asguard.py
+++ asguard/asguard.py
@@ -1,7 +1,7 @@
 from subprocess import PIPE, Popen
 
 def getTracerouteResult(target):
-    command = './malas/asroute.sh ' + target + ' NO'
+    command = './asguard/asroute.sh ' + target + ' NO'
     process = Popen(command, stdout=PIPE, stderr=None, shell=True)
     output = process.communicate()[0].decode("utf-8")
     output = str(output).replace("\n", "").split(" ")
