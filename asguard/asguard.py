b70df58c457abb10d88849e78fe60637feb2a811
whitel <heriskent@gmail.com>
2022-11-16 22:37:35 +0800

    抽象asguard为ASGuard class

--- asguard/asguard.py
+++ asguard/asguard.py
@@ -1,8 +1,12 @@
 from subprocess import PIPE, Popen
 
-def getTracerouteResult(target):
-    command = './asguard/asroute.sh ' + target + ' NO'
-    process = Popen(command, stdout=PIPE, stderr=None, shell=True)
-    output = process.communicate()[0].decode("utf-8")
-    output = str(output).replace("\n", "").split(" ")
-    return output
+class ASGuard:
+    def __init__(self):
+        pass
+
+    def getTracerouteResult(self, target):
+        command = './asguard/asroute.sh ' + target + ' NO'
+        process = Popen(command, stdout=PIPE, stderr=None, shell=True)
+        output = process.communicate()[0].decode("utf-8")
+        output = str(output).replace("\n", "").split(" ")
+        return output
